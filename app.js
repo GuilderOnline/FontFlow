aceaef45f3f59dcd9556673c07c38c19f3f9fd36
GuilderOnline <niall@guilderdesign.com>
2025-07-29 09:54:53 +0100

    railway app

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(cors({
   origin: [
     "http://localhost:5173", // local dev
     "https://your-frontend.vercel.app", // production frontend
-    "https://your-frontend.up.railway.app" // if hosting frontend on Railway
+    "https://fontflow-production.up.railway.app/" // if hosting frontend on Railway
   ],
   credentials: true
 }));
